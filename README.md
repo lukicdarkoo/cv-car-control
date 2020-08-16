44b6b8aed32bd4e7fff0e40967ca25a49bd7d488
Darko Lukic <lukicdarkoo@gmail.com>
2020-08-16 02:09:00 +0200

    Add steering wheel

--- README.md
+++ README.md
@@ -10,8 +10,8 @@ labelImg
 
 In `labelImg` make sure to click `YOLO` and annotate all the images.
 Once all the images annotated move them, together with the corresponding `*.txt` files, to `dataset/train` and `dataset/valid`.
-It is very important to move 10-20 percent `dataset/valid` directory and the rest, move to `dataset/train`.
-YOLO needs list of images for validation and training written to files.
+It is very important to move 10-20 percent of the images to `dataset/valid` directory and the rest, move to `dataset/train`.
+YOLO also needs a list of images for validation and training written to `*.txt` files.
 You can make the files by invoking the following commands from the project's root:
 ```bash
 cd ./dataset
@@ -21,18 +21,36 @@ find valid -name '*.jpg' > valid.txt
 
 ### Start training locally
 
-Download pretrained weights for the transfer learning:
+If you have GPU and CUDA available then you may prefer to train the YOLO locally.
+
+First, install `darknet` with GPU and CUDA support:
+```bash
+
+git clone https://github.com/pjreddie/darknet
+cd darknet
+sed -i 's/CUDNN=0/CUDNN=1/g' darknet/Makefile
+sed -i 's/GPU=0/GPU=1/g' darknet/Makefile
+make -j8
+```
+
+To accelerate training it is usually a good idea to start from pre-trained model.
+Download the pre-trained model as:
 ```bash
 wget https://pjreddie.com/media/files/darknet53.conv.74
 ```
 
-Install `darknet`
-
+Finally, you can initiate the training as:
 ```bash
 cd ./dataset
 $HOME/darknet/darknet detector train ./custom.data ../yolov3-tiny_train.cfg ../darknet53.conv.74 
 ```
+and your models will be available in `./dataset/backup`.
 
 ### Start training on Google Colab
 
-[![Google Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/drive/1xQ2eHRcVIB8GCBsp85jxWabH-tavZIE3?usp=sharing)
+In case you don't have GPU it probably a good idea to use [![Google Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/drive/1xQ2eHRcVIB8GCBsp85jxWabH-tavZIE3?usp=sharing).
+After the Google Colab is ready, upload `yolov3-tiny_train.cfg` and `./dataset` folder compressed as `dataset.tar.xz`.
+
+> You can create `dataset.tar.xz` as `tar -cf ./dataset ./dataset.tar.xz`
+
+Run all cells and after 1 hour of training the model will be ready waiting in `./dataset/backup`.
--- detection.py
+++ detection.py
@@ -1,17 +1,40 @@
+import math
 import cv2
 import numpy as np
+
+
+def draw_overlay(l_img, s_img, x_offset, y_offset):
+    # Reference: https://stackoverflow.com/a/14102014/1983050
+
+    y1 = y_offset 
+    y2 = y_offset + s_img.shape[0]
+    x1 = x_offset
+    x2 = x_offset + s_img.shape[1]
+    alpha_s = s_img[:, :, 3] / 255.0
+    alpha_l = 1.0 - alpha_s
+    for c in range(0, 3):
+        l_img[y1:y2, x1:x2, c] = (alpha_s * s_img[:, :, c] +
+                                alpha_l * l_img[y1:y2, x1:x2, c])
+
+def rotate_image(image, angle):
+  image_center = tuple(np.array(image.shape[1::-1]) / 2)
+  rot_mat = cv2.getRotationMatrix2D(image_center, -angle * 180 / math.pi, 1.0)
+  result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+  return result
 
 
 cap = cv2.VideoCapture(0)
 
 # Load YOLO
 net = cv2.dnn.readNet(
-    '/home/lukic/Downloads/yolov3-tiny_train.backup', 'yolov3-tiny.cfg')
+    '/home/lukic/Downloads/yolov3-tiny_train (3).backup', 'yolov3-tiny.cfg')
 layer_names = net.getLayerNames()
 output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
+wheel_image = cv2.imread('steering_wheel.png', -1)
+
 while True:
-    ret, img = cap.read()
+    _, img = cap.read()
     image_height, image_width, channels = img.shape
 
     # Detecting objects
@@ -27,8 +50,8 @@ while True:
         for detection in out:
             confidence = detection[5]
             if confidence > 0.5:
-                center_x = detection[0] * image_width
-                center_y = detection[1] * image_height
+                center_x = int(detection[0] * image_width)
+                center_y = int(detection[1] * image_height)
                 width = int(detection[2] * image_width)
                 height = int(detection[3] * image_height)
 
@@ -37,6 +60,8 @@ while True:
                 objects.append({
                     'x': x,
                     'y': y,
+                    'center_x': center_x,
+                    'center_y': center_y,
                     'width': width,
                     'height': height,
                     'confidence': confidence
@@ -48,16 +73,17 @@ while True:
         accept_object_1 = True
         for object_2 in objects:
             if object_1['x'] < object_2['x'] + object_2['width'] and \
-                object_1['x'] + object_1['width'] > object_2['x'] and \
-                object_1['y'] < object_2['y'] + object_2['height'] and \
-                object_1['y'] + object_1['height'] > object_2['y'] and \
-                object_1['confidence'] < object_2['confidence']:
+                    object_1['x'] + object_1['width'] > object_2['x'] and \
+                    object_1['y'] < object_2['y'] + object_2['height'] and \
+                    object_1['y'] + object_1['height'] > object_2['y'] and \
+                    object_1['confidence'] < object_2['confidence']:
                 accept_object_1 = False
         if accept_object_1:
             filtered_objects.append(object_1)
 
     # Take 2 objects with the highest confidence
-    filtered_objects = sorted(filtered_objects, key=lambda x: x['confidence'], reverse=True)
+    filtered_objects = sorted(
+        filtered_objects, key=lambda x: x['confidence'], reverse=True)
     filtered_objects = filtered_objects[:2]
 
     # Visualize
@@ -67,6 +93,26 @@ while True:
         w = object['width']
         h = object['height']
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    if len(filtered_objects) == 2:
+        left_object = filtered_objects[1]
+        right_object = filtered_objects[0]
+        if filtered_objects[0]['center_x'] < filtered_objects[1]['center_x']:
+            left_object = filtered_objects[0]
+            right_object = filtered_objects[1]
+
+        #print(right_object['center_x'] - left_object['center_x'])
+        width_diff = right_object['center_x'] - left_object['center_x']
+        height_diff = right_object['center_y'] - left_object['center_y']
+        wheel_width = int(math.sqrt(width_diff**2 + height_diff**2))
+        wheel_x = (right_object['center_x'] + left_object['center_x']) / 2 - wheel_width / 2
+        wheel_x = max(min(wheel_x, img.shape[0] - wheel_width), 0)
+        wheel_y = (right_object['center_y'] + left_object['center_y']) / 2 - wheel_width / 2
+        wheel_y = max(min(wheel_y, img.shape[1] - wheel_width), 0)
+        wheel_angle = math.atan2(height_diff, width_diff)
+        wheel_transformed = cv2.resize(wheel_image, (wheel_width, wheel_width))
+        wheel_transformed = rotate_image(wheel_transformed, wheel_angle)
+        draw_overlay(img, wheel_transformed, int(wheel_x), int(wheel_y))
 
     cv2.imshow('image', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
